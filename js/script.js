7512cc6efb1123295b2ce9816c948258c695de60
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-08 19:45:09 +0000

    u

--- js/script.js
+++ js/script.js
@@ -7,16 +7,16 @@
 // function to calculate area
 function calculateArea() {
   // declaring variable for hours worked with value set as value inputed by user 
-  let sideA = parseInt(document.getElementById("sidea").value);
+  let sideA = document.getElementById("sidea").value;
 
   // declaring variable for side b inputted by user
-  let sideB = parseInt(document.getElementById("sideb").value);
+  let sideB = document.getElementById("sideb").value;
   // declaring variable for for height inputted by user 
-  let height = parseInt(document.getElementById("height").value);
+  let height = document.getElementById("height").value;
 
   // calculates result by multiplying the product of side a, side b, and height by 1/6
   let volume = (height * sideA * sideB) * 1/6;
-  document.getElementById("user-info").innerHTML = "The volume of the triangular pyramid is " + volume.toFixed(2) + " cm².";
+  document.getElementById("user-info").innerHTML = "The volume of the triangular pyramid is " + volume.toFixed(3) + " cm<sup>3</sup>.";
 
   return area;
 }
